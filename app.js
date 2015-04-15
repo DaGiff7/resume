8425721474b9deceeea02519c34ab92edce5d23c
David Gifford <david@spotcastllc.com>
2015-04-15 01:14:39 -0400

    update default page

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var http = require('http')
 var serveStatic = require('serve-static')
 
 // Serve up public/ftp folder
-var serve = serveStatic('public', {'index': ['index.html', 'index.htm', 'myResume.html']})
+var serve = serveStatic('public', {'index': ['index.html', 'index.htm']})
 
 // Create server
 var server = http.createServer(function(req, res){
